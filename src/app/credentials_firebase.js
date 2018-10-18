6fa341cfa2ae034978e6e6b6b80ef77a1df913cf
junior santos <juniorsnts123@gmail.com>
2018-10-18 17:49:20 -0300

    gitgnore att

---
+++ src/app/credentials_firebase.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyAnZGudssOcGH8dY5WbzdNkeBTMnbdtfjo",
+    authDomain: "ionic3-firebase-1b3c8.firebaseapp.com",
+    databaseURL: "https://ionic3-firebase-1b3c8.firebaseio.com/",
+    projectId: "ionic3-firebase-1b3c8",
+    storageBucket: "ionic3-firebase-1b3c8.appspot.com",
+    messagingSenderId: "181630021604"
+};
